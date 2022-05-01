18fe85aa8bfbfad553ffec8b98e7ca5c570128df
Michael Bonner <michaelwbonner@gmail.com>
2022-05-01 11:12:40 -0600

    prevent drags from opening modals

--- components/story-pin.js
+++ components/story-pin.js
@@ -18,6 +18,31 @@ const StoryPin = ({
       ? "translate3d(0,-5px,0) scale(1.15)"
       : "translate3d(0,0px,0) scale(1)",
   });
+  const [clickPosition, setClickPosition] = useState({
+    x: null,
+    y: null,
+  });
+
+  const handleClickStart = (e) => {
+    setClickPosition({
+      x: e.nativeEvent.screenX,
+      y: e.nativeEvent.screenY,
+    });
+  };
+
+  const handleClickEnd = (e) => {
+    if (
+      clickPosition.x === e.nativeEvent.screenX &&
+      clickPosition.y === e.nativeEvent.screenY
+    ) {
+      setActiveStory(story);
+      setHoveredPin(null);
+    }
+    setClickPosition({
+      x: null,
+      y: null,
+    });
+  };
 
   return (
     <animated.div
@@ -36,12 +61,15 @@ const StoryPin = ({
       <button
         className="z-30"
         href={`/?id=${id}`}
-        onClick={() => setActiveStory(story)}
-        onTouchEnd={() => setActiveStory(story)}
+        onMouseDown={handleClickStart}
+        onMouseUp={handleClickEnd}
+        onTouchStart={handleClickStart}
+        onTouchEnd={handleClickEnd}
         style={{
           width: `${pinDimensions[0]}px`,
           height: `${pinDimensions[1]}px`,
         }}
+        type="button"
       >
         <img
           alt={title}
