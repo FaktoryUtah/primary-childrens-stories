a6e6204ac9f7a3ac02df0425a877adb77480ed3d
Michael Bonner <michaelwbonner@gmail.com>
2023-11-06 10:14:38 -0700

    redirect sitemap_index.xml to sitemap.xml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/sitemap_index.xml ",
+        destination: "/sitemap.xml",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
